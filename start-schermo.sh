260465b6ec8b28c80f80546ac4ba439da1ba1677
Emanuele Paolini <emanuele.paolini@gmail.com>
2024-05-18 10:57:09 +0200

    try new run script

--- start-schermo.sh
+++ start-schermo.sh
@@ -2,12 +2,24 @@
 
 export DISPLAY=:0
 
+echo sleep 10
+
 sleep 10
+
+echo killall
 
 killall firefox-esr
 
+echo sleep 2
+
 sleep 2
 
-unclutter &
+echo unclutter
+
+unclutter >/dev/null 2>&1 &
+
+echo run firefox
 
 nohup firefox --kiosk https://unipisa.github.io/dm-monitor >/dev/null 2>&1 &
+
+echo done
